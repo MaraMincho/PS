6ed0d640533bd826a5d9bc3db31fbc9096e6f896
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-10-09 18:55:42 +0900

    update

--- boj/boj2417.py
+++ boj/boj2417.py
@@ -4,6 +4,9 @@ left = 0
 right = int(sys.maxsize ** 1/2 + 1)
 target = int(input())
 
+if target == 0 :
+    print(0)
+    exit(0)
 while left + 1 < right :
     mid = (left + right) // 2
     if mid * mid < target :
