9e964250e8d209298f2caaae9086430c4fe9a44b
MaraMincho <seemaster300@naver.com>
2024-10-19 23:55:43 +0900

    update

--- boj/boj2179.py
+++ boj/boj2179.py
@@ -1,30 +1,43 @@
 import sys
 input = sys.stdin.readline
-words = []
 
-### 제출 안함 
 n = int(input())
-[words.append(input().strip()) for _ in range(n)]
-originalWords = words[:]
+words = []
+originalWords = []
+for ind in range(n):
+    word = input().strip()
+    words.append([word, ind])
+    originalWords.append(word)
+
 words.sort()
 
-maxCount = 0
-resInd = -1
-for ind in range(n - 1) :
-    n0, n1 = words[ind], words[ind + 1]
-    
-    if maxCount + 1 < len(n0) and maxCount + 1 < len(n1) and n0[:maxCount + 1] == n1[:maxCount + 1] :
-        tempInd = 0
-        while tempInd < len(n0) and tempInd < len(n1) and n0[:tempInd + 1] == n1[:tempInd + 1] :
-            tempInd += 1
-        maxCount = tempInd
-        resInd = ind
-taregetPrefix = words[resInd][:maxCount]
-resFlag = 0
-for word in originalWords:
-    if taregetPrefix in word :
-        print(word)
-        resFlag += 1
-        if resFlag == 2 :
-            break
+maxPrefixLen = 0
+resultInd = sys.maxsize 
 
+for i in range(n - 1):
+    word1, idx1 = words[i]
+    word2, idx2 = words[i + 1]
+    
+    commonPrefixLen = 0
+    while commonPrefixLen < len(word1) and commonPrefixLen < len(word2) and word1[commonPrefixLen] == word2[commonPrefixLen]:
+        commonPrefixLen += 1
+
+    if commonPrefixLen > maxPrefixLen:
+        maxPrefixLen = commonPrefixLen
+        resultInd = min([idx1, idx2])
+
+    elif commonPrefixLen == maxPrefixLen:
+        minIdxVal = min([idx1, idx2])
+        # 입력된 순서로 더 앞선 단어를 우선적으로 선택
+        if minIdxVal < resultInd:
+            resultInd = minIdxVal
+
+# 결과 출력
+targetPrefix = originalWords[resultInd][:maxPrefixLen]
+resFlga = 0
+for word in originalWords:
+    if word[:maxPrefixLen] == targetPrefix:
+        print(word)
+        resFlga += 1
+        if resFlga == 2 :
+            break
--- boj/tempCodeRunnerFile.py
+++ boj/tempCodeRunnerFile.py
@@ -1,1 +1,34 @@
-)
+import sys
+input = sys.stdin.readline
+words = []
+
+### 제출 안함 
+n = int(input())
+tempSet = set()
+for ind in range(n):
+    curInput = input().strip()
+    if curInput in tempSet :
+        continue
+    words.append([curInput, ind])
+originalWords = words[:]
+words.sort()
+maxCount = 0
+currentPrefixInd = -1
+resInd = sys.maxsize
+for ind in range(n - 1) :
+    n0, n0Ind, n1, n1Ind = words[ind][0], words[ind][1], words[ind + 1][0], words[ind + 1][1]
+    if maxCount + 1 < len(n0) and maxCount + 1 < len(n1) and n0[:maxCount + 1] == n1[:maxCount + 1] or (n0[:maxCount] == n1[:maxCount] and min(n0Ind, n1Ind) < currentPrefixInd ) :
+        tempInd = 0
+        while tempInd < len(n0) and tempInd < len(n1) and n0[:tempInd + 1] == n1[:tempInd + 1] :
+            tempInd += 1
+        maxCount = tempInd
+        currentPrefixInd = min(n0Ind, n1Ind)
+        resInd = ind
+taregetPrefix = words[resInd][0][:maxCount]
+resFlag = 0
+for (word, wordInd) in originalWords:
+    if taregetPrefix == word[:maxCount] :
+        print(word)
+        resFlag += 1
+        if resFlag == 2 :
+            break
