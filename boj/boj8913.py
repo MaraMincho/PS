0009acfa8937016b5bd58b7be4a0cfddb7b0b62c
MaraMincho <seemaster300@naver.com>
2024-10-25 13:36:33 +0900

    update

--- boj/boj8913.py
+++ boj/boj8913.py
@@ -37,9 +37,39 @@ def sol() :
                 q.append(nextCur)
     return 1 if flag else 0
 
+def sol2() :
+    curInput = input().strip()
+    def spiltInput(curInput: str)  :
+        curStr = curInput[0]
+        res = []
+        for ele in curInput[1:] :
+            if curStr[0] == ele :
+                curStr += ele
+            else :
+                res.append(curStr)
+                curStr = ele
+        res.append(curStr)
+        return res
+    visited = collections.defaultdict(lambda: False)
+
+    def dfs(strList: list) :
+        strList = spiltInput("".join(strList))
+        if len(strList) == 1 :
+            return 0 if len(strList[0]) == 1 else 1
+        for ind in range(len(strList)) :
+            if len(strList[ind]) >= 2 :
+                newList = strList[:]
+                newList.pop(ind)
+                if visited[tuple(newList)] == False :
+                    visited[tuple(newList)] = True
+                    curRes = dfs(newList)
+                    if curRes == 1 :
+                        return 1
+        return 0
+    return dfs(spiltInput(curInput))
 
 res = []
 for _ in range(int(input())) :
-    res.append(sol())
+    res.append(sol2())
 
 [print(x) for x in res]
