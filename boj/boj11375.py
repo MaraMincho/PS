0f03f94e7cf35393ca6ab06768a37f73d870fee6
MaraMincho <seemaster300@naver.com>
2024-10-28 00:15:10 +0900

    update

--- boj/boj11375.py
+++ boj/boj11375.py
@@ -4,9 +4,7 @@ input = sys.stdin.readline
 
 peopleCount, taskCount = list(map(int, input().split()))
 
-tasks = [[0]]
-for ind in range(1, peopleCount + 1) :
-    tasks.append(list(map(int, input().split()[1:])))
+tasks = [[0]] + [list(map(int, input().split()))[1:] for _ in range(peopleCount)]
     
 assigned_tasks = [0] + [-1] * taskCount
 visited = []
@@ -25,4 +23,4 @@ for ind in range(1, peopleCount + 1) :
     visited = [False] * (taskCount + 1)
     if dfs(ind) :
        res += 1
-print(res) 
+print(res)
