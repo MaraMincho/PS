3dbe25ad80d6e99357fd7506956a726ede770ef8
MaraMincho <seemaster300@naver.com>
2024-11-02 23:23:41 +0900

    update

--- boj/boj25822.py
+++ boj/boj25822.py
@@ -2,49 +2,46 @@ import sys
 input = sys.stdin.readline
 
 C = float(input())
-K = int(C / 0.99)
-K = K if K <= 2 else 2
+K = min(int(C // 0.99), 2)
 KDays = []
 N = int(input())
 days = list(map(int, input().split()))
+if days[0] == 0 and K > 0 :
+    KDays.append(0)
 
-left = 0
-right = 0
-maxLength = 1
-maxProblemCount = 0
-if days[0] == 0 :
-    if len(KDays) < K :
-        KDays.append(0)
-
+left, right = 0 , 0
+maxLength, maxProblemCount = 0, max(days)
 while right < N :
-    while right + 1 < N:
-        if days[right + 1] > 0 :
+    while right + 1 < N :
+        if days[right + 1] != 0 :
             right += 1
         elif len(KDays) < K :
+            KDays.append(right + 1)
             right += 1
-            KDays.append(right)
         else :
             break
-    currentSequenceLength = right - left + 1
-    if maxLength <= currentSequenceLength :
-        targetList = list(map(lambda x: days[x], range(left, right + 1)))
-        if maxLength == currentSequenceLength :
-            maxProblemCount = max([maxProblemCount] + targetList)
-        else :
-            maxProblemCount = max(targetList)
-        maxLength = currentSequenceLength
-
+    # K = 0이면서,  left, right 가 같으며 days[left] == 0일 때
+    if days[left] == 0 and left not in KDays :
+        left += 1
+        right = left
+        continue
+    
+    currentLength = right - left + 1
+    if currentLength > maxLength:
+        maxLength = currentLength
+    
     if left in KDays :
         KDays.remove(left)
-
+    
     left += 1
-    if left < N and days[left] == 0 and len(KDays) < K and left not in KDays:
+    if left < N and days[left] == 0 and left not in KDays and len(KDays) < K:
         KDays.append(left)
-
+    
     if right < left :
         right = left
+
 print(maxLength)
-print(maxProblemCount)
+print(max(days))
 
 # 3
 # 7
@@ -53,7 +50,13 @@ print(maxProblemCount)
 # 3
 # 7
 # 0 9 0 0 0 20 0
+# answer
+# 3
+# 20
 
 # 1
 # 7
 # 0 20 0 0 1 2 0
+# answer 
+# 3
+# 2
--- boj/tempCodeRunnerFile.py
+++ boj/tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-print(left, right)
+
