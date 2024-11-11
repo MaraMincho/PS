c0ee9b41c9d6f395a02274d68091e364167245f4
MaraMincho <seemaster300@naver.com>
2024-11-11 18:26:28 +0900

    upudate

---
+++ boj/swea2806.py
@@ -0,0 +1,34 @@
+import sys
+import collections
+input = sys.stdin.readline
+def sol() :
+    N = int(input())
+    
+    colVisited = collections.defaultdict(bool)
+    leftVisited = collections.defaultdict(bool)
+    rightVisited = collections.defaultdict(bool)
+    res = 0
+    def next(depth) :
+        nonlocal res
+        if depth == N :
+            res += 1
+            return
+        
+        for col in range(N) :
+            leftKey = depth + col
+            rightKey = depth - col
+            if colVisited[col] or leftVisited[leftKey] or rightVisited[rightKey]:
+                continue
+            colVisited[col] = True
+            leftVisited[leftKey] = True
+            rightVisited[rightKey] = True
+            next(depth + 1)
+            colVisited[col] = False
+            leftVisited[leftKey] = False
+            rightVisited[rightKey] = False
+    next(0)
+    print(res)
+
+for ind in range(int(input())) :
+    print(f"#{ind + 1}", end= " ")
+    sol()
