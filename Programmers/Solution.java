b8d2b87c8aa168cbf5eafafb8208b18eb944926f
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2025-01-09 23:42:08 +0900

    update

--- Programmers/Solution.java
+++ Programmers/Solution.java
@@ -1,17 +1,10 @@
-import java.util.Scanner;
-import java.util.stream.IntStream;
-
-public class Solution {
+class Solution {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        StringBuilder res = new StringBuilder();
-        
-        IntStream.range(0,a.length()).forEach(ind -> {
-            var cur = a.charAt(ind);
-            var curRes = Character.isUpperCase(cur) ? Character.toLowerCase(cur) : Character.toUpperCase(cur);
-            res.append(curRes);
-        });
-        System.err.println(res.toString());
+        System.out.println(solution("Program29b8UYP"	,"merS123",	7));
+    }
+
+    public static String solution(String my_string, String overwrite_string, int s) {
+        String answer = my_string.substring(0, s) + overwrite_string + my_string.substring(s + overwrite_string.length(), my_string.length());
+        return answer.toString();
     }
 }
