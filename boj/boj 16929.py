e303753c2f4ef8b1af1fe7bea44741a35123722d
MaraMincho <seemaster300@naver.com>
2025-01-21 13:51:34 +0900

    update

--- boj/boj 16929.py
+++ boj/boj 16929.py
@@ -1,19 +1,36 @@
 import sys
+sys.setrecursionlimit(10**6)  # 재귀 깊이 제한 증가
 input = sys.stdin.readline
 
-def sol() :
-    N, M = list(map(int, input().split()))
-    board = list(map(lambda _: list(map(str, input().strip())), range(N)))
-    
+def sol():
+    N, M = map(int, input().split())
+    board = [list(input().strip()) for _ in range(N)]
+
     dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     
-    def next(row: int, col: int, dirInd: int) :
+    visited = [[False] * M for _ in range(N)]  
+    
+    def dfs(x, y, start_x, start_y, count):
+        visited[x][y] = True
         
-        
-    for row in range(N):
-        for col in range(M):
-            pass;    
+        for dx, dy in dirs:
+            nx, ny = x + dx, y + dy
+            
+            if 0 <= nx < N and 0 <= ny < M and board[nx][ny] == board[x][y]:
+                if nx == start_x and ny == start_y and count >= 4:
+                    return True
+                if not visited[nx][ny]:
+                    if dfs(nx, ny, start_x, start_y, count + 1):
+                        return True
+    
+        visited[x][y] = False
+        return False
+
+    for i in range(N):
+        for j in range(M):
+            if not visited[i][j]:
+                if dfs(i, j, i, j, 1):
+                    return True
     return False
 
-print(sol())
-    
+print("Yes" if sol() else "No")
