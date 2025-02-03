2bc2361192e75deb3d252e9a2061b61399bdc862
MaraMincho <seemaster300@naver.com>
2025-02-03 23:22:02 +0900

    update

--- Programmers/Solution.java
+++ Programmers/Solution.java
@@ -1,21 +1,43 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 class Solution {
-    public int[] solution(int[] arr, int[][] queries) {
-        List<Integer> answer = new ArrayList<Integer>();
-        for (var value: queries) {
-            int s = value[0];
-            int e = value[1];
-            int k = value[2];
-            int curAnswer = -1;
-            for (int ind = s; ind <= e; ind += 1) {
-                if (k < arr[ind] && (curAnswer == -1 ? 1_000_000 : curAnswer) > arr[ind]) {
-                    curAnswer = arr[ind];
+    public int solution(int n, int[][] lighthouse) {
+         int answer = 0;
+        List<Set<Integer>> list = new ArrayList<>();
+        for(int idx = 0; idx <= n; idx++) {
+            list.add(new HashSet<>());
+        }
+        for(int[] lh : lighthouse) {
+            list.get(lh[0]).add(lh[1]);
+            list.get(lh[1]).add(lh[0]);
+        }
+        int[] visit = new int[n + 1];
+        dfs(list, 1, visit);
+
+        for(int v : visit) {
+            if(v == 2) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+    public void dfs(List<Set<Integer>> list, int pos, int[] visit) {
+        visit[pos] = 1;
+        Set<Integer> set = list.get(pos);
+        for(int way : set) {
+            if(visit[way] == 0) {
+                dfs(list, way, visit);
+                if(visit[way] == 1) {
+                    visit[pos] = 2;
                 }
             }
-            answer.add(curAnswer);
         }
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+    public static void main(String[] args) {
+        Solution cur = new Solution();
+        cur.solution(8, null)
     }
 }
