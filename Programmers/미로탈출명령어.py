bafef79e7ec865fa8422e697c7335723f47ec712
MaraMincho <seemaster300@naver.com>
2025-02-04 14:55:15 +0900

    update

--- Programmers/미로탈출명령어.py
+++ Programmers/미로탈출명령어.py
@@ -1,57 +1,59 @@
-# d(아래), l(왼쪽), r(오른쪽), u(위쪽)
 def solution(n, m, x, y, r, c, k):
-    dirs = [("d", (1, 0)), ('l', (0, -1)), ('r', (0, 1)), ('u', (-1, 0))]
-    s = (x, y)
-    e = (r, c)
-    
-    # 구해야 할 것들
-    # 이동하는 최적의 루트
-    # 현 자리에서 왔다리 갔다리 할 수 있는지(중요)
-    # Impossible한지
-    
-    def getD(s, e):
-        return abs(s[0] - e[0]) + abs(s[1] - e[1])
-    def isOk(cur):
-        nonlocal n, m
-        return 1 <= cur[0] <= n and 1 <= cur[1] <= m
-    e0 = getD(s,e )
-    
-    # Impossible 로직
-    if k < e0 or (k - e0) % 2 == 1:
+    directions = [("d", (1, 0)), ("l", (0, -1)), ("r", (0, 1)), ("u", (-1, 0))]
+    start, end = (x, y), (r, c)
+
+    def distance(p1, p2):
+        return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+    def is_valid(pos):
+        return 1 <= pos[0] <= n and 1 <= pos[1] <= m
+
+    min_steps = distance(start, end)
+
+    # Impossible 조건 체크
+    if k < min_steps or (k - min_steps) % 2 == 1:
         return "impossible"
-    
-    # e0 list 구하기 
-    e1Count = (k - e0) // 2
-    
-    cur = s
-    answer = ""
-    def re(cur):
-        nonlocal answer, e1Count
-        for (curString, dir) in dirs :
-            flag = False
-            while True :
-                prevAbs = getD(cur, e)
-                next = (cur[0] + dir[0], cur[1] + dir[1])
-                nextAbs = getD(next, e)
-                if isOk(next) and nextAbs <= prevAbs :
-                    flag = True
-                    answer += curString
-                    cur = next
-                elif isOk(next) and (e1Count) > 0 :
-                    flag = True
-                    e1Count -= 1
-                    answer += curString
-                    cur = next
-                else:
+
+    extra_moves = (k - min_steps) // 2
+    current = start
+    path = ""
+
+    def move(cur):
+        nonlocal path, extra_moves
+        for direction, (dx, dy) in directions:
+            moved = False
+            while True:
+                next_pos = (cur[0] + dx, cur[1] + dy)
+
+                # 다음 위치가 유효하지 않으면 탈출
+                if not is_valid(next_pos):
                     break
-                # 남은 거리가 존재하면서, "r", 이나 "u"는 다시 solution loop을 돌아야만 합니다. 
-                rest = (k - len(answer)) - getD(cur, e)
-                if not rest == 0 and curString in ["r", "u"]:
+
+                prev_dist = distance(cur, end)
+                next_dist = distance(next_pos, end)
+
+                # 최적 경로를 따르지 않는다면, 정크 무빙 기회가 없으면 중단
+                if next_dist > prev_dist and extra_moves <= 0:
                     break
-            if flag :
+
+                # 정크 무빙이라면, 기회 차감
+                if next_dist > prev_dist:
+                    extra_moves -= 1
+
+                moved = True
+                path += direction
+                cur = next_pos
+
+                # 남은 거리 확인 (정확히 k만큼 이동해야 함)
+                remaining_moves = (k - len(path)) - distance(cur, end)
+                if remaining_moves != 0 and direction in ["r", "u"]:
+                    break
+
+            if moved:
                 return cur
         return cur
-    while not len(answer) == k:
-        cur = re(cur)
-    
-    return answer
+
+    while len(path) < k:
+        current = move(current)
+
+    return path
