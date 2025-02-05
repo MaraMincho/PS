a12a425a8290961a7f027c0eab7d625a0c040940
MaraMincho <seemaster300@naver.com>
2025-02-05 10:11:02 +0900

    update

--- boj/boj11657.py
+++ boj/boj11657.py
@@ -1,31 +1,33 @@
 import sys
 input = sys.stdin.readline
-N, M = list(map(int, input().split()))
 
-board = [list(map(lambda x: sys.maxsize, range(N + 1))) for _ in range(N + 1)]
+(N, M) = list(map(int, input().split()))
 
-for ind in range(N + 1):
-    board[ind][ind] = 0
 
+board = list(map(lambda _ : sys.maxsize, range(N + 1)))
+prev = list(map(lambda _ : None, range(N + 1)))
+edges = []
 for _ in range(M) :
-    x, y, w = list(map(int, input().split()))
-    board[x][y] = min(board[x][y], w)
+    v0, v1, w = list(map(int, input().split()))
+    edges.append((v0, v1, w))
     
-for i in range(N + 1):
-    for s in range(N + 1) :
-        for e in range(N + 1) :
-            if board[s][i] + board[i][e] < board[s][e] :
-                board[s][e] = board[s][i] + board[i][e]
-copiedBoard = list(map(lambda x: x[:], board))
 
-for i in range(N + 1):
-    for s in range(N + 1) :
-        for e in range(N + 1) :
-            if board[s][i] + board[i][e] < board[s][e] :
-                print(-1)
-                exit(0)
-
-res = copiedBoard[1][2:]
-for cur in res :
-    print( cur if cur != sys.maxsize else -1)
+def bf(start) :
+    board[start] = 0
     
+    for ind in range(N) :
+        for edge in edges :
+            prev, next, w = edge
+            
+            if not board[prev] == sys.maxsize and board[next] > board[prev] + w :
+                board[next] = board[prev] + w
+                if ind == N - 1 :
+                    return []
+    return board
+
+res = bf(1)
+if res :
+    res = list(map(lambda x: x if not x == sys.maxsize else -1, res))
+    list(map(lambda x: print(x), res[2:]))
+else :
+    print(-1)
